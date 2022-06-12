4dacf881a6bf8a0f34d1c9747e1aa96720378696
Mg <gekoncze@seznam.cz>
2022-06-12 08:12:42 +0200

    ...

--- test/cz/mg/vulkantransformator/AllTests.java
+++ test/cz/mg/vulkantransformator/AllTests.java
@@ -1,12 +1,13 @@
 package cz.mg.vulkantransformator;
 
 import cz.mg.annotations.classes.Test;
-import cz.mg.vulkantransformator.services.parser.preprocessor.DefinitionManagerTest;
-import cz.mg.vulkantransformator.services.parser.segmentation.LineParserTest;
 import cz.mg.vulkantransformator.services.parser.matcher.PatternMatcherTest;
+import cz.mg.vulkantransformator.services.parser.preprocessor.DefinitionManagerTest;
+import cz.mg.vulkantransformator.services.parser.preprocessor.ErrorParserTest;
+import cz.mg.vulkantransformator.services.parser.preprocessor.PreprocessorTest;
+import cz.mg.vulkantransformator.services.parser.segmentation.LineParserTest;
 import cz.mg.vulkantransformator.services.parser.segmentation.StatementParserTest;
 import cz.mg.vulkantransformator.services.parser.segmentation.TokenParserTest;
-import cz.mg.vulkantransformator.services.parser.preprocessor.PreprocessorTest;
 import cz.mg.vulkantransformator.services.parser.splicer.SplicerTest;
 
 public @Test class AllTests {
@@ -17,6 +18,7 @@ public @Test class AllTests {
         PatternMatcherTest.main(args);
         PreprocessorTest.main(args);
         DefinitionManagerTest.main(args);
+        ErrorParserTest.main(args);
         SplicerTest.main(args);
     }
 }
